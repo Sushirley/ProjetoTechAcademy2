5176c38d8361383ce56df8e4cfbb69f9b0e72baa
Shirley <shirley.layy@gmail.com>
2025-06-01 22:14:11 -0300

    Alterações cores

--- JS/Menu.js
+++ JS/Menu.js
@@ -5,9 +5,9 @@ toggle.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
 
-// Fecha o menu clicando fora dele
 document.addEventListener('click', (e) => {
   if (!toggle.contains(e.target) && !menu.contains(e.target)) {
     menu.classList.remove('active');
   }
 });
+
